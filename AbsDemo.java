3057ae72f60a27f5a19d6a15c5808caff9bc8968
Huzaifa-Antule <120238440+Huzaifa-Antule@users.noreply.github.com>
2023-01-11 15:25:49 +0530

    Abstract Classes Java

--- AbsDemo.java
+++ AbsDemo.java
@@ -1,9 +1,33 @@
-abstract class Abs//abstract class
-{
-	 abstract static void m1();
+abstract class Abs{
+	abstract void m1();
+	void m2(){
+	 System.out.println("In m2() method of Abs ");
+	}
+	//abstract static void m1();
 	 //illegal combination of modifiers: abstract and static
-	 abstract final void m1();
+	//abstract final void m1();
 	 //illegal combination of modifiers: abstract and final
-	 abstract private void m1();
+	//abstract private void m1();
 	//illegal combination of modifiers: abstract and private
+
+}
+class Abs1 extends Abs{
+	void m1(){
+		System.out.println("In m1() method of Abs ");
+	}
+}
+
+class AbsDemo{
+  public static void main(String args[]){
+     
+	 Abs1 obj = new Abs1();
+	 obj.m1();
+	 obj.m2();
+  
+  
+  
+  
+  
+  }
+
 }
