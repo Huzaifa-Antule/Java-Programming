a628275e1f384735de720cbaa91566de8d1e6e19
Huzaifa-Antule <120238440+Huzaifa-Antule@users.noreply.github.com>
2023-01-12 11:09:08 +0530

    Packages Demonstration.

--- InterDemo.java
+++ InterDemo.java
@@ -1,40 +1,74 @@
-interface Inter1
+interface ShapeD
 {
-	void m1();
-}
-interface Inter2 
-{
-	void m2();
-}
-interface Inter3 extends Inter1,Inter2
-{
-	void m3();
-}
-class A  implements Inter3
-{
-	public void m1()
-	{
-		System.out.println ("in m1() implements in A class");
-	}
-
-	public void m2()
-	{
-		System.out.println ("in m2() implements in A class");
-	}	
-
-	public void m3()
-	{
-		System.out.println ("in m3() implements in A class");
-	}
+	double PI=3.14;
+	void area();
 }	
+
+class Circle implements ShapeD
+{
+	double r;
+	
+	Circle(double r)
+	{
+		this.r=r;
+	}
+	
+	public void area()
+	{
+		System.out.println ("Area of Circle "+(PI*r*r));
+	}
+	
+}
+
+class Square implements ShapeD
+{
+	double s;
+	
+	Square(double s)
+	{
+		this.s=s;
+	}
+	
+	public void area()
+	{
+		System.out.println ("Area of Square "+(s*s));
+	}	
+}
+
+class Rect implements ShapeD
+{
+	double l,b;
+	
+	Rect(double l,double b)
+	{
+		this.l=l;
+		this.b=b;
+	}
+	
+	public void area()
+	{
+		System.out.println ("Area of Rectangle "+(2*l*b));
+	}	
+}
 
 class InterDemo
 {
 	public static void main(String args[])
 	{
-		A obj=new A();
-		obj.m1();
-		obj.m2();
-		obj.m3();
+		ShapeD s;
+		
+		s=new Circle(2);
+		s.area();
+		
+		System.out.println ("-----------");
+		
+		s=new Square(3);
+		s.area();
+		
+		System.out.println ("-----------");
+			
+		s=new Rect(2,3);
+		s.area();
+		
 	}
 }
