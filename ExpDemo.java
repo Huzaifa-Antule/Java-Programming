ca7795fde7135cce69dd71e0e669966367703c5e
Huzaifa-Antule <120238440+Huzaifa-Antule@users.noreply.github.com>
2023-01-19 12:22:43 +0530

    User Define Exception.

--- ExpDemo.java
+++ ExpDemo.java
@@ -1,29 +1,36 @@
 import java.util.*;
+
+class CannotDivideBySevenException extends Exception
+{
+	CannotDivideBySevenException(String msg)
+	{
+		System.out.println (msg);
+	}
+}
 class ExpDemo
 {
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
 		try
+		{
+			System.out.println("Enter Value a");
+			int a=sc.nextInt();
+			System.out.println("Enter Value b");
+			int b=sc.nextInt();
+			
+			if(b==7)
 			{
-				System.out.println("Enter Value a");
-				int a=sc.nextInt();
-				System.out.println("Enter Value b");
-				int b=sc.nextInt();
-				
-				int c=a/b;
-				System.out.println("Result = "+c);
-			}
-			catch(Throwable e)
-			{
-				System.out.println(e.getMessage());
-				System.out.println (e);
-				e.printStackTrace();
-			}
-			finally
-			{
-				System.out.println ("in finally block");
-			}
-			System.out.println("End of Main");
+				throw new CannotDivideBySevenException("cannot divide by Seven");
+			}				
+			int c=a/b;
+			System.out.println ("Result = "+c)	;
+		}
+		catch(CannotDivideBySevenException cdbse)
+		{
+			System.out.println (cdbse);
+		}
+			
+		System.out.println("End of Main");
 	}
 }
